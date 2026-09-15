fd5e1aa9c9a0dc694da0d1d1acfe21553d8018b5
Bosang6 <huangzhikang66@gmail.com>
2026-09-15 14:47:41 +0200

    Add HUDWidget

---
+++ Source/ScaredyImp/Public/UI/HUDWidget.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDWidget.generated.h"
+
+UCLASS()
+class SCAREDYIMP_API UHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
