0fd3ed01d3264bb68d56523dda5458e6c9f59faa
Bosang6 <huangzhikang66@gmail.com>
2026-09-16 22:34:44 +0200

    Integrate BossStatusWidget into HUDWidget

--- Source/ScaredyImp/Private/UI/HUDWidget.cpp
+++ Source/ScaredyImp/Private/UI/HUDWidget.cpp
@@ -2,6 +2,8 @@
 
 #include "UI/HUDWidget.h"
 #include "UI/PlayerStatusWidget.h"
+#include "UI/BossStatusWidget.h"
+#include "Enemies/EnemyBase.h"
 #include "ScaredyImpCharacter.h"
 
 void UHUDWidget::BindToCharacter(AScaredyImpCharacter* Character)
@@ -16,4 +18,27 @@ void UHUDWidget::UnbindFromCharacter()
 	if (!IsValid(PlayerStatusWidget)) return;
 
 	PlayerStatusWidget->UnbindFromCharacter();
+}
+
+void UHUDWidget::ShowBossStatus(AEnemyBase* Boss)
+{
+	if (!IsValid(BossStatusWidget) || !IsValid(Boss)) return;
+
+	BossStatusWidget->BindToBoss(Boss);
+	BossStatusWidget->SetVisibility(ESlateVisibility::HitTestInvisible);
+}
+
+void UHUDWidget::HideBossStatus()
+{
+	if (!IsValid(BossStatusWidget)) return;
+
+	BossStatusWidget->UnbindFromBoss();
+	BossStatusWidget->SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UHUDWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	HideBossStatus();
 }
--- Source/ScaredyImp/Public/UI/HUDWidget.h
+++ Source/ScaredyImp/Public/UI/HUDWidget.h
@@ -7,6 +7,8 @@
 
 class AScaredyImpCharacter;
 class UPlayerStatusWidget;
+class AEnemyBase;
+class UBossStatusWidget;
 
 UCLASS()
 class SCAREDYIMP_API UHUDWidget : public UUserWidget
@@ -15,11 +17,18 @@ class SCAREDYIMP_API UHUDWidget : public UUserWidget
 
 public:
 	void BindToCharacter(AScaredyImpCharacter* Character);
-
 	void UnbindFromCharacter();
+
+	void ShowBossStatus(AEnemyBase* Boss);
+	void HideBossStatus();
+
+protected:
+	virtual void NativeConstruct() override;
 
 protected:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UPlayerStatusWidget> PlayerStatusWidget;
-	
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UBossStatusWidget> BossStatusWidget;
 };
