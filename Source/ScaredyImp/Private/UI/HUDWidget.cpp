160f2f9fa6dd243583cdbee05892aae722b28c22
Bosang6 <huangzhikang66@gmail.com>
2026-09-16 14:42:44 +0200

    Forward character binding to PlayerStatusWidget

--- Source/ScaredyImp/Private/UI/HUDWidget.cpp
+++ Source/ScaredyImp/Private/UI/HUDWidget.cpp
@@ -1,4 +1,19 @@
 
 
 #include "UI/HUDWidget.h"
+#include "UI/PlayerStatusWidget.h"
+#include "ScaredyImpCharacter.h"
 
+void UHUDWidget::BindToCharacter(AScaredyImpCharacter* Character)
+{
+	if (!IsValid(PlayerStatusWidget)) return;
+
+	PlayerStatusWidget->BindToCharacter(Character);
+}
+
+void UHUDWidget::UnbindFromCharacter()
+{
+	if (!IsValid(PlayerStatusWidget)) return;
+
+	PlayerStatusWidget->UnbindFromCharacter();
+}
--- Source/ScaredyImp/Public/UI/HUDWidget.h
+++ Source/ScaredyImp/Public/UI/HUDWidget.h
@@ -5,9 +5,21 @@
 #include "Blueprint/UserWidget.h"
 #include "HUDWidget.generated.h"
 
+class AScaredyImpCharacter;
+class UPlayerStatusWidget;
+
 UCLASS()
 class SCAREDYIMP_API UHUDWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void BindToCharacter(AScaredyImpCharacter* Character);
+
+	void UnbindFromCharacter();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UPlayerStatusWidget> PlayerStatusWidget;
 	
 };
